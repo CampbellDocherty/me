1577af366f8ae7a4c09ab778775ffa2eaa34c558
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-13 16:19:39 +0000

    Wips

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,76 +1,9 @@
-import { styled } from 'styled-components';
-import then from '../../assets/then.png';
-import now from '../../assets/now.png';
 import { useState } from 'react';
+import { Section, ImageContainer, ImageButton, WorkInProgress } from './styles';
+import { Wip } from './Wip';
 import { wips } from './wips';
-
-const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  padding: 20px;
-  text-align: right;
-  border: 1px solid white;
-  align-items: flex-end;
-  width: 80%;
-
-  & > p {
-    width: 80%;
-  }
-`;
-
-const ImageContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 16px;
-`;
-
-const ImageButton = styled.button<{ $expanded: boolean }>`
-  width: ${(props) => (props.$expanded ? '30%' : '20%')};
-  transition: width 0.3s ease-in-out;
-  background-color: black;
-  padding: 0;
-  outline: none;
-  border: none;
-  cursor: pointer;
-
-  &::selection {
-    background-color: transparent;
-  }
-
-  & > img {
-    width: 100%;
-    transition: width 0.3s ease-in-out;
-  }
-`;
-
-const WorkInProgress = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  overflow: scroll;
-  gap: 18px;
-  font-size: 0.9rem;
-  margin-top: 16px;
-`;
-
-const WipProject = styled.div`
-  align-self: flex-end;
-  width: 95%;
-  flex: 1;
-  max-height: 30px;
-  border-bottom: 1px solid white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-
-  & > p {
-    margin: 0;
-  }
-`;
+import now from '../../assets/now.png';
+import then from '../../assets/then.png';
 
 export const About = () => {
   const [expanded, setExpanded] = useState<string | null>(null);
@@ -95,12 +28,7 @@ export const About = () => {
       <p>Software engineer, technologist + (sometime) photographer!</p>
       <WorkInProgress>
         {wips.map((wip) => {
-          return (
-            <WipProject key={wip.title}>
-              <p>{wip.title}</p>
-              <p>+</p>
-            </WipProject>
-          );
+          return <Wip key={wip.title} wip={wip} />;
         })}
       </WorkInProgress>
     </Section>
---
+++ src/components/about/styles.ts
@@ -0,0 +1,83 @@
+import { styled } from 'styled-components';
+
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 20px;
+  text-align: right;
+  border: 1px solid white;
+  align-items: flex-end;
+  width: 80%;
+
+  & > p {
+    width: 80%;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const ImageButton = styled.button<{ $expanded: boolean }>`
+  width: ${(props) => (props.$expanded ? '30%' : '20%')};
+  transition: width 0.3s ease-in-out;
+  background-color: black;
+  padding: 0;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &::selection {
+    background-color: transparent;
+  }
+
+  & > img {
+    width: 100%;
+    transition: width 0.3s ease-in-out;
+  }
+`;
+
+export const WorkInProgress = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: scroll;
+  gap: 18px;
+  font-size: 0.9rem;
+  margin-top: 16px;
+`;
+
+export const WipProject = styled.div`
+  align-self: flex-end;
+  width: 95%;
+
+  border-bottom: 1px solid white;
+  padding-bottom: 8px;
+
+  cursor: pointer;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & > p {
+    margin: 0;
+  }
+`;
+
+export const Content = styled.div<{ $isOpen: boolean }>`
+  transition: max-height 1s ease;
+  max-height: ${(props) => (props.$isOpen ? '500px' : '0px')};
+  justify-self: center;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  align-items: flex-start;
+`;
