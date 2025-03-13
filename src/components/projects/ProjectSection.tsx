36a93f447000972cd9cf92a32f3bea9096464d64
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-13 18:29:25 +0000

    Close sections when others open

--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -1,11 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Project } from './Project';
 import { projects } from './projects';
 import { Container } from './styles';
 import { Project as ProjectType } from './types';
+import { Sections } from '../../App';
 
-export const ProjectSection = () => {
+export const ProjectSection = ({
+  onOpen,
+  openSection,
+}: {
+  openSection: Sections | null;
+  onOpen: (section: Sections | null) => void;
+}) => {
   const [openProject, setOpenProject] = useState<ProjectType | null>(null);
+
+  useEffect(() => {
+    if (openSection) {
+      if (openSection !== Sections.PROJECTS) {
+        setOpenProject(null);
+      }
+    }
+  }, [openSection]);
+
+  const onProjectOpen = (p: ProjectType | null) => {
+    if (p && openSection !== Sections.PROJECTS) {
+      onOpen(Sections.PROJECTS);
+    }
+    setOpenProject(p);
+  };
+
   return (
     <Container>
       {projects.map((project, i) => (
@@ -13,7 +36,7 @@ export const ProjectSection = () => {
           key={i}
           project={project}
           openProject={openProject}
-          onOpen={(p: ProjectType | null) => setOpenProject(p)}
+          onOpen={onProjectOpen}
         />
       ))}
     </Container>
